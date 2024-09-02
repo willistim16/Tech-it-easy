a8697a4a3d409d3029a2f4cb5e27707cb070c0b1
willistim16 <willistim16@gmail.com>
2024-09-02 10:40:31 +0200

    Eerste commit tech-it-easy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import './App.css';
+import {inventory} from "./constants/inventory.js";
 
 function App() {
+
+    const TvTypes = inventory.map(() => {
+        return inventory.type
+    })
+
+    // console.log(TvTypes)
   return (
+      <>
     <h1>Begin hier met met maken van de applicatie!</h1>
+          <ul>
+              {inventory.map(() => {
+                  return (<li key={inventory.type} className={}></li>)
+              })}
+          </ul>
+      </>
   )
 }
 
